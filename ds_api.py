6e5ea8a6964dd91776ad4ffa20a8ab0da0f9c02c
lin667z <2577342101@qq.com>
2025-05-10 20:29:10 +0800

    first_submit

--- ds_api.py
+++ ds_api.py
@@ -8,8 +8,8 @@ def clean_text(original_text):
 
 def ds_api(query,token_num):
     client = OpenAI(
-        api_key="",
-        base_url="https://api-hcya75k62e723cf6.aistudio-app.com/v1"
+        api_key="d776ff5cfe72d5273a6db512f269a5e8fa2c1dcd",
+        base_url="https://api-c1n3w031y882w2h7.aistudio-app.com/v1"
     )
 
     completion = client.chat.completions.create(
